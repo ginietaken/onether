6178f8e046d0893a657a4bb1904ca64906a52400
0uhca <georgeokangaonline@gmail.com>
2022-05-05 14:39:15 +0100

    Adding services page to  main

--- js/main.js
+++ js/main.js
@@ -18,3 +18,20 @@ selectElement(".hamburger").addEventListener("click", () => {
     navToggle.style.color = "#ffffff";
   }
 });
+
+// Services
+window.onscroll = function (e) {
+  let constantY = 700;
+  let scrollTop =
+    window.pageYOffset ||
+    (document.documentElement || document.body.parentNode || document.body)
+      .scrollTop;
+  let sidebars = document.getElementsByClassName("sidebar");
+  if (scrollTop > constantY) {
+    sidebars[0].classList.add("sticky-panel");
+  } else {
+    sidebars[0].classList.remove("sticky-panel");
+  }
+
+  //console.log(window.pageYOffset || (document.documentElement || document.body.parentNode ||document.body).scrollTop);
+};
